958af972f6e7f7d4a316fabda9120e0b6399256a
rodrigo <rodrigo.mrosa@hotmail.com>
2026-05-03 17:07:11 -0300

    Expand training assets

--- app/constants/enums/asset_enum.py
+++ app/constants/enums/asset_enum.py
@@ -4,7 +4,10 @@ import enum
 class AssetEnum(enum.Enum):
     BTC_USD: str = "BTC-USD"
     ETH_USD: str = "ETH-USD"
-    LTC_USD: str = "LTC-USD"
+    BNB_USD: str = "BNB-USD"
+    USDT_USD: str = "USDT-USD"
     XRP_USD: str = "XRP-USD"
+
+    LTC_USD: str = "LTC-USD"
     ADA_USD: str = "ADA-USD"
     SOL_USD: str = "SOL-USD"
